8766fd35d7dd3783f8e82b7fba74d1ccfc36935f
Luis Fernando <luiferpalmera@gmail.com>
2019-07-06 16:13:46 -0500

    Calculadora terminada

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.calculadora;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -36,10 +37,61 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calcular(View v){
-        n1 = Double.parseDouble(num1.getText().toString());
-        n2 = Double.parseDouble(num2.getText().toString());
-        suma = n1 + n2;
-        resultado.setText(""+suma);
+        double res = 0;
+        int opc;
+        if(validar()){
+            n1 = Double.parseDouble(num1.getText().toString());
+            n2 = Double.parseDouble(num2.getText().toString());
+            opc = combo.getSelectedItemPosition();
 
+            switch (opc){
+                case 0:
+                    res = n1 + n2;
+                    break;
+                case 1:
+                    res = n1 - n2;
+                    break;
+                case 2:
+                    res = n1 * n2;
+                    break;
+                case 3:
+                    res = n1 / n2;
+                    break;
+
+            }
+            //debug
+            //Log.d("Prueba", "Valor del resultado" + res);
+            resultado.setText(""+res);
+        }
+
+
+    }
+
+    public void borrar(View v){
+        num1.setText("");
+        num1.setError(null);
+        num2.setText("");
+        num2.setError(null);
+        combo.setSelection(0);
+        num1.requestFocus();
+        resultado.setText("");
+    }
+
+    public boolean validar(){
+        int opc = combo.getSelectedItemPosition();
+        if(num1.getText().toString().isEmpty()){
+            num1.setError(getResources().getString(R.string.error_1));
+            num1.requestFocus();
+            return false;
+        }else if(num2.getText().toString().isEmpty()){
+            num2.setError(getResources().getString(R.string.error_2));
+            num2.requestFocus();
+            return false;
+        }else if(Double.parseDouble(num2.getText().toString()) == 0 && opc == 3){
+            num2.setError(getResources().getString(R.string.error_3));
+            num2.selectAll();
+            return false;
+        }
+        return true;
     }
 }
